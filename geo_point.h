942bb470f128c364c7442d267e9ffbed6fc230eb
Григорук Екатерина <144429737+ekaterinagrigoruk@users.noreply.github.com>
2024-05-19 22:38:27 +0700

    Update geo_point.h

--- geo_point.h
+++ geo_point.h
@@ -8,16 +8,13 @@
 class GeoPoint final {
 public:
 
-    GeoPoint(double lat = 0, double lng = 0, double elev = 0, time_t t = 0)
+    GeoPoint(double lat, double lng, double elev, time_t t)
             : latitude(lat), longitude(lng), elevation(elev), time(t) {}
-    [[nodiscard]] double get_latitude() const { return latitude; }
-    [[nodiscard]] double get_longitude() const { return longitude; }
-    [[nodiscard]] double get_elevation() const { return elevation; }
-    [[nodiscard]] time_t get_time() const { return time; }
-    [[nodiscard]] double distance(const GeoPoint &other) const;
-    bool operator < (const GeoPoint& other) const {
-        return time < other.time;
-    }
+    inline double get_latitude() const { return latitude; }
+    inline double get_longitude() const { return longitude; }
+    inline double get_elevation() const { return elevation; }
+    inline time_t get_time() const { return time; }
+    double distance(const GeoPoint &other) const;
 private:
     double latitude;
     double longitude;
